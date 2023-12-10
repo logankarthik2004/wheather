898c4ca6bd3f55d2512ab533d3813597dabf27d5
logankarthik2004 <153418682+logankarthik2004@users.noreply.github.com>
2023-12-10 12:55:01 +0530

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,70 @@
+const searchInput = document.querySelector("#searchInput")
+searchButton = document.querySelector("#searchButton")
+weatherIcon = document.querySelector("#weatherIcon")
+windSpeed = document.querySelector("#windSpeed")
+humidity = document.querySelector(".humidity")
+weather = document.querySelector(".weather")
+desc = document.querySelector(".desc")
+API = "88b6dedcd76c80aefafb673fa941065b";
+
+const setWeatherDetails = (data) => {
+    // console.log(data);
+    desc.innerHTML = data.weather[0].description;
+    weather.innerHTML = Math.round(data.main.temp - 273.15) + "°c";
+    humidity.innerHTML = data.main.humidity + "%";
+    windSpeed.innerHTML = data.wind.speed + "km/h";
+    switch (data.weather[0].main) {
+        case 'Clouds':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+        case 'Clear':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+        case 'Rain':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+        case 'Mist':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+        case 'Snow':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+        case 'Haze':
+            weatherIcon.src = "forecastsun.gif";
+            break;
+    }
+}
+
+const callAPI = (id) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${id}`)
+        .then(response => {
+            // indicates whether the response is successful (status code 200-299) or not
+            if (!response.ok) {
+                alert("Check spelling of City and try again or Something Went Wrong!");
+                throw new Error(`Request failed with status ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(data => {
+            setWeatherDetails(data);
+        })
+        .catch(error => console.log(error))
+}
+
+searchButton.addEventListener("click", (e) => {
+    if (searchInput.value == "") {
+        alert("Please Enter City Name.");
+    } else {
+        callAPI(API);
+    }
+})
+
+searchInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        searchButton.click();
+    }
+})
+
+searchButton.click();
+
